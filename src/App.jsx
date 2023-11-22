716acf550bda0ce79caa7ca0787dd0626fa250de
jottaveDev <jviktor204@gmail.com>
2023-11-21 21:56:09 -0300

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { UserStorage } from './userContext';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
+import './App.css';
+import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hooks/useForm.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const types = {
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Preencha um email válido!',
+  },
+  senha: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z$*&@#]{8,}$/,
+    message:
+      'A senha precisa ter no minimo 8 caracteres. Contendo maiusculo, minusculo e número.',
+  },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize números apenas',
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function onChange({ target }) {
+    // if (error) validate(target.value);
+    setError(null);
+    setValue(target.value);
+  }
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor!');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
